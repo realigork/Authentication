df9167243335c966752000d6be90105666d7fde4
Igor Kolga <igorkolga@hotmail.com>
2018-07-20 14:39:36 +0100

    Add db connection and authenticaion controller with user data

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const http = require('http');
 const bodyParser = require('body-parser'); // parse incoming requests (to json)
 const morgan = require('morgan'); // logging framework
 const router = require('./router');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Setup
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
 
 // App Setup
 app.use(morgan('combined'));
