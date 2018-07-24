a69b2c5d224080f8d5a51ff432b73a28266ece16
Igor Kolga <igorkolga@hotmail.com>
2018-07-24 19:21:10 +0100

    Update Header links to display based on the authenticated prop; Add bit of styling to links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './HeaderStyle.css';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/feature">Feature</Link>
+          <Link to="/signout">Sign out</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign up</Link>
+          <Link to="/signin">Sign in</Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return(
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign up</Link>
-        <Link to="/signin">Sign in</Link>
-        <Link to="/signout">Sign out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect(mapStateToProps)(Header);
