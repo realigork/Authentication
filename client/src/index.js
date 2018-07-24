7df99153b656c0d192d77e4f5fc3beacf2d2c04b
Igor Kolga <igorkolga@hotmail.com>
2018-07-24 16:59:49 +0100

    Add initial auth reducer and wrap the app with Provider and createStore

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" component={Welcome} exact />
-      <Route path="/signup" component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" component={Welcome} exact />
+        <Route path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
